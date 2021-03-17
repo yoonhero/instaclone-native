23a580e0150c67208285cbca79a1716ddf14d93b
yoonhero <yoonhero06@naver.com>
2021-03-18 07:54:22 +0900

    #1.1 navigator

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
+import { NavigationContainer } from "@react-navigation/native";
 import AppLoading from "expo-app-loading";
-import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import { Ionicons } from "@expo/vector-icons";
 import * as Font from "expo-font";
-import { StyleSheet, Text, View } from "react-native";
+import { Asset } from "expo-asset";
+import LoggedOutNav from "./navigators/LoggedOutNav";
 
 export default function App() {
   const [loading, setLoading] = useState(true);
@@ -11,8 +12,12 @@ export default function App() {
   const preload = () => {
     const fontToLoad = [Ionicons.font];
     const fontPromises = fontToLoad.map((font) => Font.loadAsync(font));
-    console.log(fontPromises);
-    return Promise.all(fontPromises);
+    const imagesToLoad = [
+      require("./assets/logo.png"),
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1600px-Instagram_logo.svg.png",
+    ];
+    const imagePromises = imagesToLoad.map((image) => Asset.loadAsync(image));
+    return Promise.all(...fontPromises, ...imagePromises);
   };
   if (loading) {
     return (
@@ -24,18 +29,8 @@ export default function App() {
     );
   }
   return (
-    <View style={styles.container}>
-      <Text>OK just do it</Text>
-      <StatusBar style='auto' />
-    </View>
+    <NavigationContainer>
+      <LoggedOutNav />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
---
+++ navigators/LoggedOutNav.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Welcome } from "../screens/Welcome";
+import { LogIn } from "../screens/Login";
+import { CreateAccount } from "../screens/CreateAccount";
+
+const Stack = createStackNavigator();
+
+export default function LoggedOutNav() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name='Welcome' component={Welcome} />
+      <Stack.Screen name='LogIn' component={LogIn} />
+      <Stack.Screen name='CreateAccount' component={CreateAccount} />
+    </Stack.Navigator>
+  );
+}
