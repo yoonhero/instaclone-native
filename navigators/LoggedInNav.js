1dbb4a1038bb0c1e29ad52d22afd4a0f6be4a637
yoonhero <yoonhero06@naver.com>
2021-03-29 12:37:13 +0900

    #2.2 camera tab & select photo

--- navigators/LoggedInNav.js
+++ navigators/LoggedInNav.js
@@ -1,84 +1,16 @@
 import React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Feed from "../screens/Feed";
-import Search from "../screens/Search";
-import Notification from "../screens/Notification";
-import { Image, View } from "react-native";
-import TabIcon from "../components/nav/TabIcon";
-import Me from "../screens/Me";
-import SharedStackNav from "../components/SharedStackNav";
-import useMe from "../hooks/useMe";
+import TabsNav from "./TabsNav";
+import { createStackNavigator } from "@react-navigation/stack";
+import SelectPhoto from "../screens/SelectPhoto";
+import UploadNav from "./UploadNav";
 
-const Tabs = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 export default function LoggedInNav() {
-  const { data } = useMe();
   return (
-    <Tabs.Navigator
-      tabBarOptions={{
-        activeTintColor: "white",
-        showLabel: false,
-        style: {
-          borderTopColor: "rgba(255, 255, 255, 0.3)",
-          backgroundColor: "black",
-        },
-      }}>
-      <Tabs.Screen
-        name='Feed'
-        options={{
-          tabBarIcon: ({ focused, color, size }) => (
-            <TabIcon iconName={"home"} color={color} focused={focused} />
-          ),
-        }}>
-        {() => <SharedStackNav screenName='Feed' />}
-      </Tabs.Screen>
-      <Tabs.Screen
-        name='Search'
-        options={{
-          tabBarIcon: ({ focused, color, size }) => (
-            <TabIcon iconName={"search"} color={color} focused={focused} />
-          ),
-        }}>
-        {() => <SharedStackNav screenName='Search' />}
-      </Tabs.Screen>
-      <Tabs.Screen
-        name='Camera'
-        component={View}
-        options={{
-          tabBarIcon: ({ focused, color, size }) => (
-            <TabIcon iconName={"camera"} color={color} focused={focused} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name='Notification'
-        options={{
-          tabBarIcon: ({ focused, color, size }) => (
-            <TabIcon iconName={"heart"} color={color} focused={focused} />
-          ),
-        }}>
-        {() => <SharedStackNav screenName='Notification' />}
-      </Tabs.Screen>
-      <Tabs.Screen
-        name='Me'
-        options={{
-          tabBarIcon: ({ focused, color, size }) =>
-            data?.me?.avatar ? (
-              <Image
-                source={{ uri: data.me.avatar }}
-                style={{
-                  height: 20,
-                  width: 20,
-                  borderRadius: 10,
-                  ...(focused && { borderColor: "white", borderWidth: 1 }),
-                }}
-              />
-            ) : (
-              <TabIcon iconName={"person"} color={color} focused={focused} />
-            ),
-        }}>
-        {() => <SharedStackNav screenName='Me' />}
-      </Tabs.Screen>
-    </Tabs.Navigator>
+    <Stack.Navigator headerMode='none' mode='modal'>
+      <Stack.Screen name='Tabs' component={TabsNav} />
+      <Stack.Screen name='Upload' component={UploadNav} />
+    </Stack.Navigator>
   );
 }
---
+++ screens/SelectPhoto.js
@@ -0,0 +1,122 @@
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import * as MediaLibrary from "expo-media-library";
+import styled from "styled-components/native";
+import { useEffect } from "react";
+import { useState } from "react";
+import { FlatList, Image, TouchableOpacity } from "react-native";
+import useWindowDimensions from "react-native/Libraries/Utilities/useWindowDimensions";
+import { colors } from "../colors";
+
+const Container = styled.View`
+  flex: 1;
+  background-color: black;
+`;
+
+const Top = styled.View`
+  flex: 1;
+  background-color: black;
+`;
+
+const Bottom = styled.View`
+  flex: 1;
+  background-color: black;
+`;
+
+const ImageContainer = styled.TouchableOpacity``;
+const IconContainer = styled.View`
+  position: absolute;
+  bottom: 5px;
+  right: 0;
+`;
+
+const HeaderRightText = styled.Text`
+  color: ${colors.blue};
+  font-size: 16px;
+  font-weight: 600;
+  margin-right: 7px;
+`;
+
+export default function SelectPhoto({ navigation }) {
+  const [ok, setOk] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const [chosenPhoto, setChosenPhoto] = useState("");
+  const getPhotos = async () => {
+    const { assets: photos } = await MediaLibrary.getAssetsAsync();
+    setPhotos(photos);
+    setChosenPhoto(photos[0]?.uri);
+  };
+
+  const getPermissions = async () => {
+    const {
+      accessPrivileges,
+      canAskAgain,
+    } = await MediaLibrary.getPermissionsAsync();
+
+    if (accessPrivileges === "none" && canAskAgain) {
+      const { accessPrivileges } = await MediaLibrary.requestPermissionsAsync();
+      if (accessPrivileges !== "none") {
+        setOk(true);
+        getPhotos();
+      }
+    } else if (accessPrivileges !== "none") {
+      setOk(true);
+      getPhotos();
+    }
+  };
+
+  useEffect(() => {
+    getPermissions();
+  }, []);
+  const HeaderRight = () => (
+    <TouchableOpacity>
+      <HeaderRightText>Next</HeaderRightText>
+    </TouchableOpacity>
+  );
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: HeaderRight,
+    });
+  }, []);
+
+  const numColumns = 4;
+  const { width } = useWindowDimensions();
+  const choosePhoto = (uri) => {
+    setChosenPhoto(uri);
+  };
+  const renderItem = ({ item: photo }) => (
+    <ImageContainer onPress={() => choosePhoto(photo.uri)}>
+      <Image
+        source={{ uri: photo.uri }}
+        style={{ width: width / numColumns, height: width / numColumns }}
+      />
+      <IconContainer>
+        <Ionicons
+          name='checkmark-circle'
+          size={18}
+          color={photo.uri === chosenPhoto ? colors.blue : "white"}
+        />
+      </IconContainer>
+    </ImageContainer>
+  );
+  return (
+    <Container>
+      <Top>
+        {chosenPhoto !== "" ? (
+          <Image
+            source={{ uri: chosenPhoto }}
+            style={{ width, height: "100%" }}
+          />
+        ) : null}
+      </Top>
+      <Bottom>
+        <FlatList
+          data={photos}
+          numColumns={numColumns}
+          keyExtractor={(photo) => photo.id}
+          renderItem={renderItem}
+        />
+      </Bottom>
+    </Container>
+  );
+}
