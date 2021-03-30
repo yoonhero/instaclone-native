a81ab52fd3392f212531e6ae2baa005cdfd883e2
yoonhero <yoonhero06@naver.com>
2021-03-30 17:06:43 +0900

    #2.5 room finish

--- screens/Room.js
+++ screens/Room.js
@@ -1,14 +1,27 @@
-import { useQuery } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import gql from "graphql-tag";
 import React from "react";
 import { useEffect } from "react";
 import { FlatList, KeyboardAvoidingView, Text, View } from "react-native";
 import ScreenLayout from "../components/ScreenLayout";
 import styled from "styled-components/native";
+import { useForm } from "react-hook-form";
+import { Ionicons } from "@expo/vector-icons";
+import useMe from "../hooks/useMe";
+
+const SEND_MESSAGE_MUTATION = gql`
+  mutation sendMessage($payload: String!, $roomId: Int, $userId: Int) {
+    sendMessage(payload: $payload, roomId: $roomId, userId: $userId) {
+      ok
+      id
+    }
+  }
+`;
 
 const ROOM_QUERY = gql`
   query seeRoom($id: Int!) {
     seeRoom(id: $id) {
+      id
       messages {
         id
         payload
@@ -22,14 +35,11 @@ const ROOM_QUERY = gql`
   }
 `;
 const MessageContiainer = styled.View`
-  margin: 5px;
   padding: 0px 10px;
-  flex-direction: row;
+  flex-direction: ${(props) => (props.outGoing ? "row-reverse" : "row")};
   align-items: flex-end;
 `;
-const Author = styled.View`
-  margin-right: 10px;
-`;
+const Author = styled.View``;
 const Avatar = styled.Image`
   height: 20px;
   width: 20px;
@@ -46,35 +56,114 @@ const Message = styled.Text`
   overflow: hidden;
   border-radius: 10px;
   font-size: 16px;
+  margin: 0px 10px;
 `;
 const TextInput = styled.TextInput`
-  margin-bottom: 50px;
-  margin-top: 25px;
-  width: 95%;
   border: 2px solid rgba(255, 255, 255, 0.5);
   padding: 10px 20px;
   color: white;
   border-radius: 1000px;
+  width: 90%;
+  margin-right: 10px;
 `;
+const InputContainer = styled.View`
+  flex-direction: row;
+  width: 95%;
+  margin-bottom: 50px;
+  margin-top: 25px;
+  align-items: center;
+`;
+const SendButton = styled.TouchableOpacity``;
+
 export default function Room({ route, navigation }) {
+  const { data: meData } = useMe();
+  const { register, setValue, handleSubmit, getValues, watch } = useForm();
+
+  const updateSendMessage = (cache, result) => {
+    const {
+      data: {
+        sendMessage: { ok, id },
+      },
+    } = result;
+    if (ok && meData) {
+      const { message } = getValues();
+      setValue("message", "");
+      const messageObj = {
+        id,
+        payload: message,
+        user: {
+          username: meData.me.username,
+          avatar: meData.me.avatar,
+        },
+        read: true,
+        __typename: "Message",
+      };
+      const messageFragment = cache.writeFragment({
+        fragment: gql`
+          fragment NewMessage on Message {
+            id
+            payload
+            user {
+              username
+              avatar
+            }
+            read
+          }
+        `,
+        data: messageObj,
+      });
+      const ROOM_ID = `Room:${route.params.id}`;
+      cache.modify({
+        id: ROOM_ID,
+        fields: {
+          messages(prev) {
+            return [...prev, messageFragment];
+          },
+        },
+      });
+    }
+  };
+  const [sendMessageMutation, { loading: sendingMessage }] = useMutation(
+    SEND_MESSAGE_MUTATION,
+    {
+      update: updateSendMessage,
+    }
+  );
+
   const { data, loading } = useQuery(ROOM_QUERY, {
     variables: {
       id: route?.params?.id,
     },
   });
+  const onValid = ({ message }) => {
+    if (!sendingMessage) {
+      sendMessageMutation({
+        variables: {
+          payload: message,
+          roomId: route?.params?.id,
+        },
+      });
+    }
+  };
+  useEffect(() => {
+    register("message", { required: true });
+  }, [register]);
   useEffect(() => {
     navigation.setOptions({
       title: `Conversation with ${route?.params?.talkingTo?.username}`,
     });
   }, []);
   const renderItem = ({ item: message }) => (
-    <MessageContiainer>
+    <MessageContiainer
+      outGoing={message.user.username !== route?.params?.talkingTo?.username}>
       <Author>
         <Avatar source={{ uri: message.user.avatar }} />
       </Author>
       <Message>{message.payload}</Message>
     </MessageContiainer>
   );
+  const messages = [...(data?.seeRoom?.messages ?? [])];
+  messages.reverse();
   return (
     <KeyboardAvoidingView
       style={{ flex: 1, backgroundColor: "black" }}
@@ -83,17 +172,37 @@ export default function Room({ route, navigation }) {
       <ScreenLayout loading={loading}>
         <FlatList
           inverted
-          style={{ width: "100%" }}
-          data={data?.seeRoom?.messages}
+          showsVerticalScrollIndicator={false}
+          style={{ width: "100%", marginVertical: 10 }}
+          ItemSeparatorComponent={() => <View style={{ height: 20 }}></View>}
+          data={messages}
           keyExtractor={(message) => "" + message.id}
           renderItem={renderItem}
         />
-        <TextInput
-          placeholderTextColor='rgba(255, 255, 255, 0.5)'
-          placeholder='Write a message ...'
-          returnKeyLabel='Send Message'
-          returnKeyType='send'
-        />
+        <InputContainer>
+          <TextInput
+            placeholderTextColor='rgba(255, 255, 255, 0.5)'
+            placeholder='Write a message ...'
+            returnKeyLabel='Send Message'
+            returnKeyType='send'
+            onChangeText={(text) => setValue("message", text)}
+            onSubmitEditing={handleSubmit(onValid)}
+            value={watch("message")}
+          />
+          <SendButton
+            onPress={handleSubmit(onValid)}
+            disabled={!Boolean(watch("message"))}>
+            <Ionicons
+              name='send'
+              color={
+                !Boolean(watch("message"))
+                  ? "rgba(255, 255, 255, 0.5)"
+                  : "white"
+              }
+              size={22}
+            />
+          </SendButton>
+        </InputContainer>
       </ScreenLayout>
     </KeyboardAvoidingView>
   );
